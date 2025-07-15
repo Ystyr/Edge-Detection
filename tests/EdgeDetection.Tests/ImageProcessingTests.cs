1fe45d84b12ffedce0967e2cdd245f2168261ebc
Ystyr <black.raven@mail.ru>
2025-07-15 22:51:28 +0800

    added: unit tests

---
+++ tests/EdgeDetection.Tests/ImageProcessingTests.cs
@@ -0,0 +1,37 @@
+﻿using EdgeDetection.Core;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using static EdgeDetection.Core.EdgeDetector;
+
+namespace EdgeDetection.Tests
+{
+    public class ImageProcessingTests
+    {
+        string pathToSamples = "../../../../../assets/";
+
+        [Fact]
+        public void EdgeDetector_ShouldProcessImageWithoutErrors ()
+        {
+            var image = new Image<Rgba32>(10, 10);
+            image.Mutate(ctx => ctx.BackgroundColor(Color.White));
+
+            var result = EdgeDetector.Run(image, OperatorType.Sobel);
+
+            Assert.NotNull(result);
+            Assert.Equal(image.Width, result.Width);
+            Assert.Equal(image.Height, result.Height);
+        }
+
+        [Fact]
+        public void EdgeDetector_ShouldProcessSampleImage ()
+        {
+            using var image = Image.Load<Rgba32>(pathToSamples + "fishPie.jpg");
+
+            var result = EdgeDetector.Run(image, OperatorType.Sobel);
+
+            Assert.NotNull(result);
+            Assert.Equal(image.Width, result.Width);
+        }
+    }
+}
---
+++ tests/EdgeDetection.Tests/OperatorSelectionTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using EdgeDetection.Core;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using static EdgeDetection.Core.EdgeDetector;
+
+namespace EdgeDetection.Tests
+{
+    public class OperatorSelectionTests
+    {
+        [Fact]
+        public void Run_ShouldSelectSobelKernel ()
+        {
+            var input = new Image<Rgba32>(10, 10);
+            var result = EdgeDetector.Run(input, OperatorType.Sobel);
+
+            Assert.NotNull(result);
+            Assert.Equal(input.Width, result.Width);
+            Assert.Equal(input.Height, result.Height);
+        }
+
+        [Fact]
+        public void Run_ShouldSelectPrewittKernel ()
+        {
+            var input = new Image<Rgba32>(10, 10);
+            var result = EdgeDetector.Run(input, OperatorType.Prewitt);
+
+            Assert.NotNull(result);
+            Assert.Equal(input.Width, result.Width);
+            Assert.Equal(input.Height, result.Height);
+        }
+    }
+}
