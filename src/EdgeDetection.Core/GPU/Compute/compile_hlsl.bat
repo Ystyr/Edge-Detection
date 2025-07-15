8daae5a6b12dcfdb6c13318803567a4a33834380
Ystyr <black.raven@mail.ru>
2025-07-15 22:03:13 +0800

    fixed: compile_hlsl misleading logging

--- src/EdgeDetection.Core/GPU/Compute/compile_hlsl.bat
+++ src/EdgeDetection.Core/GPU/Compute/compile_hlsl.bat
@@ -1,4 +1,4 @@
-﻿@echo off
+@echo off
 setlocal
 
 set FXC="C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\fxc.exe"
@@ -6,17 +6,23 @@ set FXC="C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\fxc.exe"
 echo Compiling all .hlsl files in %CD%
 echo.
 
+set FAILED=0
+
 for %%F in (*.hlsl) do (
     echo Compiling %%F...
-    %FXC% /T cs_5_0 /E CSMain /Fo %%~nF.cso %%F
-    if ERRORLEVEL 1 (
-        echo.
+    %FXC% /T cs_5_0 /E CSMain /Fo %%~nF.cso %%F || (
         echo [ERROR] Failed to compile %%F
-        pause
-        exit /b 1
+        set FAILED=1
     )
 )
 
 echo.
+
+if %FAILED%==1 (
+    echo Some shaders failed to compile.
+    pause
+    exit /b 1
+)
+
 echo All shaders compiled successfully.
 pause
